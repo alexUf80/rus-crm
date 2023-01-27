b4863375b3042ef44d9ef514a42da60c12928b06
Sergey <skilinlive@mail.ru>
2023-01-27 10:39:25 +0300

    вывод

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -225,7 +225,7 @@ class OrderController extends Controller
 
             $scoring_types = $this->scorings->get_types();
 
-            '<pre>';print_r($scoring_types);echo'</pre>';
+            echo'<pre>';print_r($scoring_types);echo'</pre>';
 
 //echo __FILE__.' '.__LINE__.'<br /><pre>';var_dump($scoring_types);echo '</pre><hr />';
             $this->design->assign('scoring_types', $scoring_types);
