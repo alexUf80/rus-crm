ad3f2e992844e54a441f892b95b6595aa4449491
alexUf80 <johndoe@example.comufimcev_a@mail.ru>
2023-03-30 22:54:17 +0300

    Оплата Причина отказа при отказе менеджера

--- controllers/OrderController.php
+++ controllers/OrderController.php
@@ -978,26 +978,49 @@ class OrderController extends Controller
             'user_id' => $order->user_id,
         ));
 
-        if (!empty($resp)) {
-            $resp = json_decode($resp);
 
-            $this->receipts->add_receipt(array(
-                'user_id' => $contract->user_id,
-                'Информирование о причине отказа',
-                'order_id' => $contract->order_id,
+        $defaultCard = CardsORM::where('user_id', $order->user_id)->where('base_card', 1)->first();
+
+        $resp = $this->Best2pay->purchase_by_token($defaultCard->id, 1900, 'Списание за услугу "Причина отказа"');
+        $status = (string)$resp->state;
+
+        if ($status == 'APPROVED') {
+            $this->operations->add_operation(array(
                 'contract_id' => 0,
-                'insurance_id' => 0,
-                'receipt_url' => (string)$resp->Model->ReceiptLocalUrl,
-                'response' => serialize($resp),
-                'created' => date('Y-m-d H:i:s')
+                'user_id' => $order->user_id,
+                'order_id' => $order->order_id,
+                'type' => 'REJECT_REASON',
+                'amount' => 19,
+                'created' => date('Y-m-d H:i:s'),
+                'transaction_id' => 0,
             ));
+
+            //Отправляем чек по страховке
+            $resp = $this->Cloudkassir->send_reject_reason($order->order_id);
+
+            if (!empty($resp)) {
+                $resp = json_decode($resp);
+
+                $this->receipts->add_receipt(array(
+                    'user_id' => $order->user_id,
+                    'Информирование о причине отказа',
+                    'order_id' => $order->order_id,
+                    'contract_id' => 0,
+                    'insurance_id' => 0,
+                    'receipt_url' => (string)$resp->Model->ReceiptLocalUrl,
+                    'response' => serialize($resp),
+                    'created' => date('Y-m-d H:i:s')
+                ));
+            }
         }
+
+        CardsORM::where('user_id', $order->user_id)->delete();
 
         if (!empty($order->utm_source) && $order->utm_source == 'leadstech')
             PostbacksCronORM::insert(['order_id' => $order->order_id, 'status' => 2, 'goal_id' => 3]);
 
 
-        //создаем документ на оказание услуг
+        // создаем документ на оказание услуг
         $user = $this->users->get_user($order->user_id);
         
         $params = [
