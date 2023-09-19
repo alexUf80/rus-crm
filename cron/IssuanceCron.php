b11481bbaa814534014c06036060bc16261db178
PaketMoloka <vladimir22092012@mail.ru>
2023-09-19 16:44:46 +0200

    временное отключение услуг

--- cron/IssuanceCron.php
+++ cron/IssuanceCron.php
@@ -150,7 +150,7 @@ class IssuanceCron extends Core
 
 
                     // Снимаем страховку если есть
-                    if (!empty($contract->service_insurance)) 
+                    /*if (!empty($contract->service_insurance))
                     {
                         $insurance_cost = $this->insurances->get_insurance_cost($contract->amount);
 
@@ -233,7 +233,7 @@ class IssuanceCron extends Core
                                 // }
                             }
                         }
-                    }
+                    }*/
 
                     // Снимаем смс-информирование если есть
                     $sms_cost = 149;
